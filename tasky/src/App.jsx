03b5093f4f03dd624b43829a76225398f62ca095
Yaroslav Tere <20111651@setu.ie>
2026-09-23 14:02:02 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
     const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium" },
+      { id: 3, title: "Tidy up", description: "Clean the living room", deadline: "Today", priority: "High" }
     ]
   });
 
@@ -22,6 +22,7 @@ function App() {
           title={task.title}
           description={task.description}
           deadline={task.deadline}
+          priority={task.priority}
         />
   ))} 
 
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -3,7 +3,8 @@ const Task = (props) => {
     <div className="card">
       <p className="title">{props.title}</p>
       <p>Due: {props.deadline}</p>
-      <p className="description">{props.children}</p>
+      <p className="description">{props.description}</p>
+      <h3>{props.priority}</h3>
     </div>
   )
 }
