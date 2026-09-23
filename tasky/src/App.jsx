c421bf7e0c329fcb4a37a53d27fe11ce2cf1eab9
Yaroslav Tere <20111651@setu.ie>
2026-09-23 14:17:30 +0100

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -6,23 +6,33 @@ function App() {
 
     const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium" },
-      { id: 3, title: "Tidy up", description: "Clean the living room", deadline: "Today", priority: "High" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
+      { id: 3, title: "Tidy up", description: "Clean the living room", deadline: "Today", priority: "High", done: false }
     ]
   });
+
+    const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
+
 
   return (
     <div className="container">
       <h1>Tasky</h1>
 
-      {taskState.tasks.map((task) => (
+      {taskState.tasks.map((task, index) => (
         <Task
           key={task.id}
           title={task.title}
           description={task.description}
           deadline={task.deadline}
           priority={task.priority}
+          done={task.done}
+          markDone={() => doneHandler(index)}
         />
   ))} 
 
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,10 +1,12 @@
 const Task = (props) => {
   return (
-    <div className="card">
+    <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
       <p className="title">{props.title}</p>
       <p>Due: {props.deadline}</p>
       <p className="description">{props.description}</p>
       <h3>{props.priority}</h3>
+
+      <button onClick={props.markDone}>Done</button>
     </div>
   )
 }
